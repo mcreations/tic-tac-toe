b4c9e5ce063c5e623f242226241a480160e183ca
Malinda Wijegunawardene <malinda.wijegunawardene@madmobile.com>
2024-02-18 18:53:16 +0530

    Initial repo and formating changes

--- src/components/PlayerName.jsx
+++ src/components/PlayerName.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
 
-export default function  PlayerName({initialName }) {
-const [playerName, setPlayerName] = useState(initialName)
-const [playerEdit, setPlayerEdit] = useState(false)
-function handleEditClick(){
-  setPlayerEdit(playerEdit => !playerEdit)
-}
-function handleInputChange(event){
-  setPlayerName(event.target.value)
-}
+export default function PlayerName({ initialName }) {
+  const [playerName, setPlayerName] = useState(initialName)
+  const [playerEdit, setPlayerEdit] = useState(false)
+  function handleEditClick() {
+    setPlayerEdit(playerEdit => !playerEdit)
+  }
+  function handleInputChange(event) {
+    setPlayerName(event.target.value)
+  }
+
   return (
     <div>
       {playerEdit ? <input value={playerName} onChange={handleInputChange} /> : playerName}
