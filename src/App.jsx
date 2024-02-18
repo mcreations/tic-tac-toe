7749c7b1c1125a01131029b1a9a108555727c0f2
Malinda Wijegunawardene <malinda.wijegunawardene@madmobile.com>
2024-02-18 18:09:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PlayerName from "./components/PlayerName"
+function App() {
+  
+
+  return (
+    <>
+    <h1>React Tic-Tac-Toe</h1>
+    <PlayerName initialName={"Player 1"}/>
+    <PlayerName initialName={"Player 2"}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PlayerName.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function  PlayerName({initialName }) {
+const [playerName, setPlayerName] = useState(initialName)
+const [playerEdit, setPlayerEdit] = useState(false)
+function handleEditClick(){
+  setPlayerEdit(playerEdit => !playerEdit)
+}
+function handleInputChange(event){
+  setPlayerName(event.target.value)
+}
+  return (
+    <div>
+      {playerEdit ? <input value={playerName} onChange={handleInputChange} /> : playerName}
+      <button onClick={handleEditClick}>{playerEdit ? 'Save' : 'Edit'}</button>
+    </div>
+  )
+}
